e3b4858828db48038bfde6b41e4af94842b885bd
Robert Bathory <bathory.robi@gmail.com>
2019-01-17 14:25:31 +0100

    HelloOthers

---
+++ week01/day4/src/HelloOthers.java
@@ -0,0 +1,11 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello István!");
+        System.out.println("Hello Szabi!");
+        System.out.println("Hello Vali!");
+            
+        }
+    }
+
+
+
