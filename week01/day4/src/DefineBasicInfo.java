cc5a47a9d4f5c7b5c0aba0ba88a5c0433336fe93
Robert Bathory <bathory.robi@gmail.com>
2019-01-17 16:59:03 +0100

    Basininfo

---
+++ week01/day4/src/DefineBasicInfo.java
@@ -0,0 +1,22 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Bathory Robert";
+
+        // Your age as an integer
+        int age = 36;
+
+        // Your height in meters as a double
+        double height = 1.75;
+        // Whether you are married or not as a boolean
+        boolean Marriage = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(Marriage);
+
+    }
+
+}
