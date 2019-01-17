aa101af127bc70d2d63905eb803398ca61eb80ff
Robert Bathory <bathory.robi@gmail.com>
2019-01-17 14:35:30 +0100

    PrintValues

---
+++ week01/day4/src/Values.java
@@ -0,0 +1,15 @@
+public class Values {
+    public static void main(String[] args) {
+        String name = "Báthory Róbert";
+        int number = 42;
+        double törtSzám = 3.141592;
+
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(törtSzám);
+        }
+    }
+
+
+
+
