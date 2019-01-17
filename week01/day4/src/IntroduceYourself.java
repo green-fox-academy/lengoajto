9d97da5f22b1e17e5fa060b5daf102e09988f4b3
Robert Bathory <bathory.robi@gmail.com>
2019-01-17 15:01:39 +0100

    IntroduceMyself

---
+++ week01/day4/src/IntroduceYourself.java
@@ -0,0 +1,21 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        String name = "Báthory Róbert";
+        int age = 36;
+        double height = 1.75;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+    }
+}
