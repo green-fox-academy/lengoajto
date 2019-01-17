aee189a38587c0555458b68b6e3d98f3db1fa1de
Robert Bathory <bathory.robi@gmail.com>
2019-01-17 16:00:16 +0100

    FavNUM

---
+++ week01/day4/src/FavoriteNumber.java
@@ -0,0 +1,8 @@
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 8;
+        System.out.println("My favorite number is: "+favoriteNumber);
+        // Store your favorite number in a variable (as a number)
+        // And print it like this: "My favorite number is: 8"
+    }
+}
