d87aac720214fd2f188739c2cbf91daf6d71658c
Robert Bathory <bathory.robi@gmail.com>
2019-01-18 00:12:55 +0100

    Cuboid Surface, Volume

---
+++ week01/day4/src/Cuboid.java
@@ -0,0 +1,27 @@
+public class Cuboid {
+    public static void main(String[] args) {
+
+        double lenght = 12.4;
+        double width = 20.1;
+        double height = 15.7;
+
+        double volume = lenght + width + height;
+        double surface = lenght * width * height;
+
+        System.out.println("Volume: " + volume);
+        System.out.println("Surface: " + surface);
+
+
+
+
+    }
+
+}
+
+// Write a program that stores 3 sides of a cuboid as variables (doubles)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+
