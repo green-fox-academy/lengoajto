b3f4f7b52571ca612c360d6fd57199025f498edd
Robert Bathory <bathory.robi@gmail.com>
2019-01-17 16:31:45 +0100

    SwapwithTemp

---
+++ week01/day4/src/Swap.java
@@ -0,0 +1,16 @@
+public class Swap {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
