883ed5b1bf5ba53574f0283c57fe946f3459bb1f
Robert Bathory <bathory.robi@gmail.com>
2019-01-20 16:39:12 +0100

    cheats

---
+++ week01/day4/src/IWontCheatOnTheExams.java
@@ -0,0 +1,28 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=100) {
+            System.out.println("I won't cheat on the exam!");
+            i++;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
