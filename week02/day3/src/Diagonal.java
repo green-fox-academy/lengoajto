a3520adc4fc9d0b7d6f7e419fc8664451a472dc2
Robert Bathory <bathory.robi@gmail.com>
2019-01-23 12:26:17 +0100

    Red n Green X

---
+++ week02/day3/src/Diagonal.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Diagonal {
+    public static void mainDraw(Graphics graphics) {
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(0, 0, 320, 320);
+
+        graphics.setColor(Color.RED);
+        graphics.drawLine(0, 320, 320, 0 );
+
+
+        // Draw the canvas' diagonals.
+        // If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
