7b764e83281fa01a2cd9c3d828f3fa286a0a28a1
Robert Bathory <bathory.robi@gmail.com>
2019-01-23 15:55:23 +0100

    RandomRectanglesWithRandomcolors

---
+++ week02/day3/src/FourRectangles.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Random;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        for (int i = 0; i < 4; i++) {
+            graphics.setColor(new Color(((int) (Math.random() * 255)), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+            graphics.fillRect((int) ((Math.random() * 100)), (int) ((Math.random() * 100)), (int) (Math.random() * 100), (int) ((Math.random() * 100)));
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
