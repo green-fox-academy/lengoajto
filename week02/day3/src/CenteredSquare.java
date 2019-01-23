84896faed5ee463d3a8115d9a261c354ed8df42c
Robert Bathory <bathory.robi@gmail.com>
2019-01-23 15:28:35 +0100

    The good solution for the CenteredSquare

--- week02/day3/src/CenteredSquare.java
+++ week02/day3/src/CenteredSquare.java
@@ -9,7 +9,7 @@ public class CenteredSquare {
     public static void mainDraw(Graphics graphics) {
         // Draw a green 10x10 square to the canvas' center.
         graphics.setColor(Color.GREEN);
-        graphics.drawRect((WIDTH/2-10),(HEIGHT/2-10),10,10);
+        graphics.drawRect((WIDTH/2-5),(HEIGHT/2-5),10,10);
 
 
 
