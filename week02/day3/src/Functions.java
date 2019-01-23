4247b0b8f2e098bff271630badca6b4decd349f2
Robert Bathory <bathory.robi@gmail.com>
2019-01-23 17:10:36 +0100

    PositionSquare

---
+++ week02/day3/src/Functions.java
@@ -0,0 +1,41 @@
+/*public class Functions {
+   public static void main(String[] args) {
+
+      int liszt = 15;
+       int finomliszt = 40;
+       int viz = 22;
+       int SutesEredmenye;
+
+       SutesEredmenye = Sutes(liszt, viz);
+
+       System.out.println(SutesEredmenye);
+
+
+
+
+
+       SutesEredmenye = Sutes(finomliszt, viz);
+
+       System.out.println(SutesEredmenye);
+
+
+       System.out.println(Sutes(finomliszt, viz));
+
+
+
+
+   }
+
+   public static int Sutes (int x, int y){
+       int kenyer;
+
+       kenyer = x + y;
+
+       return kenyer;
+   }
+
+
+
+
+}
+*/
---
+++ week02/day3/src/PositionSquare.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+    public static void mainDraw(Graphics graphics) {
+        // create a square drawing function that takes 3 parameters:
+        // the x and y coordinates of the square's top left corner and the graphics
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+
+        int x = 35;
+        int y = 35;
+        draw(x, y, graphics);
+    }
+    public static void draw(int x, int y, Graphics graphics) {
+        for (int i = 0; i < 3; i++) {
+            graphics.fillRect(x, y , 50, 50);
+
+            y += 60;
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+
+}
