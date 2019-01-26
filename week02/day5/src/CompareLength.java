f6e1c3ba8602f5f2422a2f72ec01ca1b57002378
Robert Bathory <bathory.robi@gmail.com>
2019-01-26 15:28:13 +0100

    CompareLenght

---
+++ week02/day5/src/CompareLength.java
@@ -0,0 +1,24 @@
+public class CompareLength {
+    public static void main(String[] args) {
+        int [] p1 = {1,2,3};
+        int [] p2 = {4,5};
+
+
+        System.out.println(p1.length < p2.length );
+    }
+
+
+
+
+}
+
+/* (number%2 == 0) ? "Even" : "Odd";
+    }
+*/
+
+
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print if `p2` has more elements than `p1`
