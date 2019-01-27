3d5b0109f9c834750fbd2ad8f125bcb28b0f3df6
Robert Bathory <bathory.robi@gmail.com>
2019-01-27 14:30:12 +0100

    Sumall

---
+++ week02/day5/src/SumAll.java
@@ -0,0 +1,19 @@
+public class SumAll {
+    public static void main(String[] args) {
+    int [] ai = {3, 4, 5, 6, 7,};
+    int sum = 0;
+
+        for (int i = 0; i < ai.length; i++) {
+            sum += ai[i];
+
+        }
+        System.out.println(sum);
+        
+
+
+    }
+}
+
+// - Create an array variable named `ai`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `ai3, 4, 5, 6, 7`
