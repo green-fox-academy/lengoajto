343d00fb91df3f44eb72fb0c5678ddfe3ea38a16
Robert Bathory <bathory.robi@gmail.com>
2019-02-26 17:36:08 +0100

    String Exercise2 is ready.

---
+++ week06/day1/src/StreamExercise2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExercise2 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        List<Integer> squaredNumbers = numbers.stream().filter(s -> s > 0).map(s -> s * s).collect(Collectors.toList());
+        System.out.println(squaredNumbers.toString());
+    }
+}
+
+
+
+
+//Write a Stream Expression to get the squared value of the positive numbers from the following list:
+//List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
